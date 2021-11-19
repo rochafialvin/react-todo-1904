4ded71b5cdf3f3a390596fe2c4c902e13be552d6
rochafialvin <rochafi.dev@gmail.com>
2021-11-19 16:05:17 +0700

    onClick handler

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 
 class TodoItem extends React.Component {
+  deleteBtnHandler() {
+    alert("Tombol delete berhasil di tekan");
+  }
+
+  completeBtnHandler(time) {
+    alert(`Good ${time}`);
+  }
+
   render() {
     return (
       <div className="d-flex w-50 p-3 justify-content-around">
         <p className="lead">Makan</p>
         <div>
-          <button className="btn btn-outline-danger me-5">Delete</button>
-          <button className="btn btn-outline-success">Complete</button>
+          <button
+            onClick={this.deleteBtnHandler}
+            className="btn btn-outline-danger me-5"
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => {
+              this.completeBtnHandler("morning");
+            }}
+            className="btn btn-outline-success"
+          >
+            Complete
+          </button>
         </div>
       </div>
     );
