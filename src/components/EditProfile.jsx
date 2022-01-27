25f97d92971119c4b96d19cf0f6debb418c617f2
rochafialvin <rochafi.dev@gmail.com>
2022-01-27 09:56:04 +0700

    Create update data user feature

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -4,10 +4,20 @@ import { useSelector } from "react-redux";
 
 export default function EditProfile() {
   const token = useSelector((state) => state.auth.token);
+  const { id, name, email } = useSelector((state) => state.auth.user);
   const [image, setImage] = useState(null);
   const [imagePreview, setImagePreview] = useState("");
+  const [formState, setFormState] = useState({
+    name: name,
+    email: email,
+    password: "",
+  });
 
-  const onEditProfile = async () => {
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const onSaveImage = async () => {
     try {
       const data = new FormData();
       data.append("photo", image);
@@ -24,6 +34,21 @@ export default function EditProfile() {
     }
   };
 
+  const onSaveData = async () => {
+    try {
+      const res = await axios.put(`/users/${id}`, formState, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      alert("Update success");
+      console.log({ res });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   const onImageChange = (e) => {
     const image = e.target.files[0];
     setImage(image);
@@ -31,14 +56,55 @@ export default function EditProfile() {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <img
-        src={imagePreview}
-        alt="preview"
-        style={{ width: 500, marginBottom: 5 }}
-      />
-      <input type="file" onChange={onImageChange} />
-      <button onClick={onEditProfile}>Save</button>
+    <div
+      style={{
+        width: "80%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <div
+        style={{ margin: "30px 0", display: "flex", flexDirection: "column" }}
+      >
+        <img
+          src={imagePreview}
+          alt="preview"
+          style={{ width: 500, marginBottom: 5 }}
+        />
+        <input type="file" onChange={onImageChange} />
+        <button onClick={onSaveImage}>Save Image</button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: 200,
+          justifyContent: "space-around",
+        }}
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={formState.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          value={formState.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={onSaveData}>Save Data</button>
+      </div>
     </div>
   );
 }
